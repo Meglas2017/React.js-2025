325cf49899fd731d428c414ec89c5efb67f4dfc9
Marcos Gerje <31744709+Meglas2017@users.noreply.github.com>
2025-09-18 01:01:45 -0300

    carrito

--- 25232/src/components/CartFloat/CartFloat.jsx
+++ 25232/src/components/CartFloat/CartFloat.jsx
@@ -4,33 +4,48 @@ import "./CartFloat.css"
 
 export default function CartFloat ({carrito,handleCartEmpty}) {
 const close = "<"
-const [show, setShow] = useState(false)
+const [show, setShow] = useState("hide")
 
 useEffect(()=>{
-    
+
 }, [show])
 
 
 const handleShowCart = () => {
-    show === false ? setShow(true) : setShow(false)
+    show === "hide" ? setShow("show") : setShow("hide")
     console.log(show)
 }
 
     return(
-        <section className="cart">
-            <div className="heatCart">
-                <div onClick={handleShowCart}>{close}</div>
-                <h3>🛒 Carrito 🛒</h3>
-            </div>
-            <ul>
-                {carrito ? carrito.map((producto, index)=>
-                    <li key={index}>{producto.product}</li>
-                ) : <></>}
-            </ul>
-            <div className="cartButtons">
-                <button onClick={handleCartEmpty}>🗑️ Vaciar carrito 🗑️</button>
-                <button>💸 Finalizar compra 💸</button>
-            </div>
-        </section>
+        // <section className={`cart ${show}`}>
+        //     <div onClick={handleShowCart} className="iconCart">
+        //         <div>{close}</div>
+        //     </div>
+        //     <div className="verticalNav">
+        //         <div>
+                    
+        //         </div>
+        //         <div className="heatCart">
+                    
+        //             <h3>🛒 Carrito 🛒</h3>
+        //         </div>
+        //         <ul>
+        //             {carrito ? carrito.map((producto, index)=>
+        //                 <li key={index}>{producto.product}</li>
+        //             ) : <></>}
+        //         </ul>
+        //         <div className="cartButtons">
+        //             <button onClick={handleCartEmpty}>🗑️ Vaciar carrito 🗑️</button>
+        //             <button>💸 Finalizar compra 💸</button>
+        //         </div>
+        //     </div>
+        // </section>
+        <>
+            
+            <section className={`cart `}>
+                <div onClick={handleShowCart} className={`iconCart `}>{close}</div>
+                <div className={`listCart ${show}`}>Lista</div>
+            </section>
+        </>
     )
 }
