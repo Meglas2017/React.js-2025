372f4c66e0af56d9097dd004db8fccb1cf2f73da
Marcos Gerje <31744709+Meglas2017@users.noreply.github.com>
2025-10-21 08:32:54 -0300

    Change
    Change to Belu´s way

--- 25232/src/App.jsx
+++ 25232/src/App.jsx
@@ -1,29 +1,30 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Clases1a3 from './pages/Clases1a3';
-import Clase04 from './pages/Clase04';
-import NewProduct from './pages/NewProduct/NewProduct';
-import Header from './Layouts/Header';
-import Foot from './Layouts/Foot';
-import Nav from './Layouts/Nav';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
+import { ItemDetailContainer } from "./components/ItemDetailContainer/ItemDetailContainer";
+import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 
 function App() {
-
-
   return (
-    <BrowserRouter>
-      <Header/>
-      <Nav/>
-      <Routes>
-        <Route path="/" element={<Clases1a3 />} />
-        <Route path="/4" element={<Clase04 />} />
-        <Route path="/newproduct" element={<NewProduct/>} />
-      </Routes>
-      <Foot/>
-    </BrowserRouter>
-
-  )
+    <>
+      <BrowserRouter>
+        <div>
+          <Header />
+          {/* Dejamos fuera del Routes lo que queremos que no se vuelva a renderizar al navegar */}
+          <Routes>
+            <Route
+              path="/"
+              element={<ItemListContainer titulo={"Bienvenidos"} />}
+            />
+            <Route path="/detail/:id" element={<ItemDetailContainer />} />
+          </Routes>
+          {/* Dejamos fuera del Routes lo que queremos que no se vuelva a renderizar al navegar */}
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
--- 25232/vite.config.js
+++ 25232/vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { visualizer} from "rollup-plugin-visualizer"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    visualizer({
-      open: true, // abre un reporte en el navegador automáticamente
-    })
-  ],
-  
+  plugins: [react()],
 })
